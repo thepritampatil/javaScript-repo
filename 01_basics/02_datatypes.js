f6b30282f96d4028b46bc46807b6f53c2a2f0cea
thepritampatil <119095038+thepritampatil@users.noreply.github.com>
2025-03-31 12:18:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all js code in newer version
+
+// alert(3 + 3) // we are using node js not browser
+console.log("pritam");
+/*console.log(3
+    +3);  // code readability should be high */
+
+    let name ="pritam" // string datatype
+    let age = 3; //int datatype
+    let isLoggedIn =true; //boolean
+
+    // number => 2 to power 53
+    //bigint 
+    //string => " "
+    // boolean => true/false
+    //null => standalone value (empty)
+   // undefined => (not assigned value)
+   //symbol =>  unique
+
+
+  //object 
+  
+  console.log(typeof"pritam");
+  console.log(typeof null );
+  
+  
+
+
+
