9935b50d7c8f95716b0dd0e65ded32026113cf49
thepritampatil <119095038+thepritampatil@users.noreply.github.com>
2025-03-31 11:56:15 +0000

    learn about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountID =122323;
+let accountEmail ="pp0743173@gmail.com";
+var accountPassword ="12322";
+accountCity ="mumbai";
+
+// accountID=2
+console.log(accountID);
+accountEmail="pp@gmail.com"
+accountPassword="3232"
+accountCity="Pune";
+let accountState;
+/*
+prefer not to use "VAR"
+because of the block scope and functional scope*/
+
+console.table([accountID ,accountEmail,accountPassword, accountCity,accountState])
+
