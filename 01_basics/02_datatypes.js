d34e4f05cd228dc65681f4c73ac3c0b9b5563bf2
thepritampatil <119095038+thepritampatil@users.noreply.github.com>
2025-04-01 10:08:38 +0000

    dataypes summary

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -5,9 +5,9 @@ console.log("pritam");
 /*console.log(3
     +3);  // code readability should be high */
 
-    let name ="pritam" // string datatype
-    let age = 3; //int datatype
-    let isLoggedIn =true; //boolean
+    // let name ="pritam" // string datatype
+    // let age = 3; //int datatype
+    // let isLoggedIn =true; //boolean
 
     // number => 2 to power 53
     //bigint 
@@ -20,10 +20,41 @@ console.log("pritam");
 
   //object 
   
-  console.log(typeof"pritam");
-  console.log(typeof null );
+//   console.log(typeof"pritam");
+//   console.log(typeof null );
   
-  
+//   ------------------ summary datatype-------------
+
+//pritmitive 
+
+// 7 types : String Number boolean , null undefined sysmbol ,BigInt
+
+// const score =100
+// const scoreValue =100.3;
+// const isLoggedIn = false;
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol ('123')
+// console.log(id === anotherId);
+
+const bigNumber = 12323432343235434323n
 
 
 
+// non-primitive ( reference type)
+
+//Arrays ,Objects ,Functions  
+
+const heros = ["shaktiman", "ironman", "captain India"];
+let myObj = {
+    name :"pritam",
+    age :22
+
+}
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+console.log(typeof bigNumber);
--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,29 +1,79 @@
-let score = 33;
+// let score = 33;
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber =Number (score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber =Number (score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 //"33abc" => NaN
 //true => 1; false => 0;
 
-let isLoggedIn = true;
+// let isLoggedIn = true;
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true ; 0=> false
 //"" => false
 // "pritam" => true
 
-let someNumber = 33;
+// let someNumber = 33;
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+
+//--------------------Operations---------------------------------
+
+// let value =3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2); 
+// console.log(2%3);//modulo (remainder)
+// console.log(2**3);//power
+
+// let str1 ="hello"
+// let str2 = " pritam"
+
+// let str3 =str1 + str2
+// console.log(str3);
+
+// console.log("1" +2);
+// console.log(1 +"2")
+// console.log("1"+ "2");
+// console.log("1"+ 2+ 2);
+// console.log(1 +2 +"3");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1 ,num2,num3
+// num1= num2 = num=3 =2 +2;
+
+let gameCounter =100;
+gameCounter ;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
 
 
 
